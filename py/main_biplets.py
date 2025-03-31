0297fc3dcb6504cbc9de64f74a418e26f98cc446
gon-zalo <147316353+gon-zalo@users.noreply.github.com>
2025-03-31 19:59:03 +0200

    Update main_biplets.py

--- py/main_biplets.py
+++ py/main_biplets.py
@@ -11,32 +11,58 @@ import transformers
 # -----------------------------------------------------
 
 # EMBEDDINGS/MODELS
-ft_spa = "py/embeddings/cc.es.300.bin" # spanish fasttext embeddings
-w2v_spa = "py/embeddings/sbw_vectors.bin" # spanish word2vec (SBW) embeddings
-mult_bert = "bert-base-multilingual-cased" # multilingual BERT language model
+# Spanish word vectors
+spa_w2v = "py/embeddings/spa/sbw_vectors.bin" # spanish word2vec (SBW) embeddings
+spa_ft = "py/embeddings/spa/cc.es.300.bin" # spanish fasttext embeddings
+
+# Polish word vectors
+pol_w2v = "py/embeddings/pol/nkjp+wiki-forms-all-100-skipg-ns"
+pol_ft = "py/embeddings/pol/cc.pl.300.bin"
+
+# Multilingual BERT model
+mult_bert = "bert-base-multilingual-cased"
 
 # DATASETS
-um_filtered_spa = pd.read_csv("py/datasets/filtered_spa.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
+um_spa = pd.read_csv("py/datasets/spa/spa_filtered.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
+um_pol = pd.read_csv("py/datasets/pol/pol_filtered.txt", sep="\t", header=None, names=["pivot", "inflection", "category"], encoding="utf-8") # utf-8 does not work for polish
 
 # -----------------------------------------------------
 
-def choose_embeddings(model_name):
+def choose_embeddings(model_name, language):
+   
+    embeddings_dict = {
+        "spa": {
+            "word2vec": spa_w2v,
+            "fasttext": spa_ft,
+        },
+        "pol": {
+            "word2vec": pol_w2v,
+            "fasttext": pol_ft,
+        }
+    }
 
-    # WORD2VEC SPANISH EMBEDDINGS
-    if model_name.lower() in ["word2vec", "w"]:
-        print('\nLoading Spanish Billion Words (Word2Vec) embeddings...')
+    embeddings = embeddings_dict[language.lower()][model_name.lower()]
+
+    if language == "spa":
+        language = "Spanish"
+    elif language == "pol":
+        language = "Polish"
+
+    if model_name.lower() == "word2vec":
+        print(f'\nLoading {language} Word2Vec embeddings...')
         # load the SBW embeddings
-        model = KeyedVectors.load_word2vec_format(w2v_spa, binary=True, limit=50000) # limit is for testing purposes
-        return model, "Word2Vec", None
+        model = KeyedVectors.load_word2vec_format(embeddings, binary=True)
+        return model, "Word2Vec", None, language
 
-    # FASTTEXT SPANISH EMBEDDINGS
-    if model_name.lower() in ["fasttext", "f"]:
-        print('\nLoading FastText embeddings...')
+    if model_name.lower() == "fasttext":
+        print(f'\nLoading {language} FastText embeddings...')
         # check if they exist, if they don't, download them
-        if not os.path.exists(ft_spa):
-            fasttext.util.download_model('es', if_exists='ignore')
-        model = fasttext.load_model(ft_spa)
-        return model, "FastText", None
+        if not os.path.exists(embeddings):
+            print(f"Downloading {language} FastText embeddings to {embeddings}...")
+            fasttext.util.download_model('es')
+            os.rename("cc.es.300.bin", embeddings)  # Move the downloaded file to the desired location
+        model = fasttext.load_model(embeddings)
+        return model, "FastText", None, language
 
     # MULTILINGUAL BERT MODEL
     if model_name.lower() in ["bert", "b"]:
@@ -49,7 +75,7 @@ def choose_embeddings(model_name):
         else:
             device='cpu'
         model.to(device)
-        return model, "BERT", tokenizer
+        return model, "BERT", tokenizer, language
 
     elif model_name.lower() == "exit":
         exit()
@@ -57,11 +83,16 @@ def choose_embeddings(model_name):
     else:
         raise ValueError("Invalid model name.")
 
-def calculate_sims(model, model_name, tokenizer,  data):
+def calculate_sims(model, model_name, tokenizer, language, data):
+    if language == "Spanish":
+        language = "spa"
+    elif language == "Polish":
+        language = "pol"
+
     # calculate cosine similarities between pivot and inflection/derivation
     print("Calculating similarities...")
     results = []
-    not_found = 0 # for SBW embeddings
+    not_found = 0 # for Word2Vec embeddings
     for _, row in data.iterrows():
         pivot, inflection = row["pivot"], row["inflection"]
 
@@ -69,14 +100,13 @@ def calculate_sims(model, model_name, tokenizer,  data):
             vec_pivot = model.get_word_vector(pivot) # vector of the pivot
             vec_inflection = model.get_word_vector(inflection) # vector of the inflection
 
-        elif model_name == "SBW":
-            
+        elif model_name == "Word2Vec":
             if pivot in model and inflection in model: # need to check since w2v does not use subwords
                 vec_pivot = model[pivot]
                 vec_inflection = model[inflection]  # Vector of the inflection
-
             else:
                 not_found += 1
+                continue
 
         elif model_name == "BERT" and tokenizer is not None:
 
@@ -99,17 +129,32 @@ def calculate_sims(model, model_name, tokenizer,  data):
 
     # create a df with the results table
     results_df = pd.DataFrame(results)
-    results_df.to_csv(f"py/results/{model_name}_results.csv", index=False, header=["pivot", "inflection", "P-I similarity"])
-
-    print(f"\n------ RESULTS OF {model_name.upper()} EMBEDDINGS ------\n")
+    results_df.to_csv(f"py/results/{language}/{language}_{model_name.lower()}_results.csv", index=False, header=["pivot", "inflection", "P-I similarity"])
+    print("Results by row saved!")
 
     # similarities for mean
     sim_inflection_values = [r[2] for r in results]
-    print(f"    Inflection mean similarity to pivot: {np.mean(sim_inflection_values):2f}")
+    print(f"{model_name.upper()} EMBEDDINGS IN {language.upper()}")
+    print(f"MEAN SIMILARITY (VERB-INFLECTION): {np.mean(sim_inflection_values):2f}")
 
 # -------------------------------------------------------
 # where functions are called
 
-model, model_name, tokenizer = choose_embeddings()  #  change the model here: fasttext (f), word2vec (w) or bert (b)
+'''
+choose_embeddings takes an argument of the model name (fasttext, word2vec or bert) and a language argument (spa or pol), it outputs the model, the model name, the tokenizer (if applicable) and the language (to be used in the results file name and file path).
 
-calculate_sims(model, model_name, tokenizer, data=um_filtered_spa) # change the data here
+calculate_sims takes what choose_embeddings outputs and an argument of the file to be used.
+'''
+# # FASTTEXT
+# model, model_name, tokenizer, language = choose_embeddings("fasttext", language="spa") # SPANISH
+# calculate_sims(model, model_name, tokenizer, language, data=um_spa)
+
+# model, model_name, tokenizer, language = choose_embeddings("fasttext", "pol") # POLISH
+# calculate_sims(model, model_name, tokenizer, language, um_pol)
+
+# # WORD2VEC
+# model, model_name, tokenizer, language = choose_embeddings("word2vec", "spa") # SPANISH
+# calculate_sims(model, model_name, tokenizer, language, um_spa)
+
+model, model_name, tokenizer, language = choose_embeddings("word2vec", "pol") # POLISH
+calculate_sims(model, model_name, tokenizer, language, um_pol)
