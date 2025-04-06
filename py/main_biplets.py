bdb1ca73957dcdddef647e26076c08daeec1908c
gon-zalo <147316353+gon-zalo@users.noreply.github.com>
2025-04-06 22:53:15 +0200

    category code

--- py/main_biplets.py
+++ py/main_biplets.py
@@ -1,4 +1,8 @@
+# Makes the current directory the path of the .py file
 import os
+import sys
+os.chdir(sys.path[0])
+
 import fasttext
 import fasttext.util
 import numpy as np
@@ -12,22 +16,22 @@ import transformers
 
 # EMBEDDINGS/MODELS
 # FASTTEXT
-spa_ft = "py/embeddings/spa/cc.es.300.bin" # spanish
-pol_ft = "py/embeddings/pol/cc.pl.300.bin" # polish
+spa_ft = "embeddings/spa/cc.es.300.bin" # spanish
+pol_ft = "embeddings/pol/cc.pl.300.bin" # polish
 
 # WORD2VEC
-spa_w2v = "py/embeddings/spa/sbw_vectors.bin" # spanish (SBW)
-pol_w2v = "py/embeddings/pol/nkjp+wiki-forms-all-300-skipg-ns.bin" # polish
+spa_w2v = "embeddings/spa/sbw_vectors.bin" # spanish (SBW)
+pol_w2v = "embeddings/pol/nkjp+wiki-forms-all-300-skipg-ns.bin" # polish
 
 # BERT models
 mult_bert = "bert-base-multilingual-cased" # multilingual bert
-herbert = "allegro/herbert-base-cased"
-polbert = "dkleczek/bert-base-polish-cased-v1"
+# herbert = "allegro/herbert-base-cased" 
+# polbert = "dkleczek/bert-base-polish-cased-v1"
 
 # DATASETS
-um_spa = pd.read_csv("py/datasets/spa/spa_filtered.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
-um_spa_small = pd.read_csv("py/datasets/spa/spa_filtered_small.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
-um_pol = pd.read_csv("py/datasets/pol/pol_filtered.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
+um_spa = pd.read_csv("datasets/spa/spa_filtered.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
+um_spa_small = pd.read_csv("datasets/spa/spa_filtered_small.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
+um_pol = pd.read_csv("datasets/pol/pol_filtered.txt", sep="\t", header=None, names=["pivot", "inflection", "category"])
 
 # -----------------------------------------------------
 
@@ -105,6 +109,7 @@ def calculate_sims(model, model_name, tokenizer, language, data):
             batch = data.iloc[i:i+batch_size]
             pivots = batch["pivot"].tolist()
             inflections = batch["inflection"].tolist()
+            categories = batch["category"].tolist()
 
             # tokenize and encode inputs (pivots and inflections)
             inputs = tokenizer(pivots + inflections, return_tensors="pt", padding=True, truncation=True) # tokenizer returns a dictionary with input_ids, attention_mask...
@@ -127,13 +132,13 @@ def calculate_sims(model, model_name, tokenizer, language, data):
             inflection_embeddings = token_embeddings[batch_size:, 0].cpu().numpy()
 
             # calculate similarities
-            for pivot, inflection, pivot_embedding, inflection_embedding in zip(pivots, inflections, pivot_embeddings, inflection_embeddings):
+            for pivot, inflection, pivot_embedding, inflection_embedding, category in zip(pivots, inflections, pivot_embeddings, inflection_embeddings, categories):
                 sim_inflection = 1 - cosine(pivot_embedding, inflection_embedding)
-                results.append((pivot, inflection, sim_inflection))
+                results.append((pivot, inflection, sim_inflection, category))
     else:
         not_found = 0 # for Word2Vec embeddings
         for _, row in data.iterrows():
-            pivot, inflection = row["pivot"], row["inflection"]
+            pivot, inflection, category = row["pivot"], row["inflection"], row["category"]
 
             if model_name == "FastText":
                 pivot_embedding = model.get_word_vector(pivot) # vector of the pivot
@@ -151,14 +156,14 @@ def calculate_sims(model, model_name, tokenizer, language, data):
             sim_inflection = 1 - cosine(pivot_embedding, inflection_embedding) # need 1 - cosine because cosine alone just measures distance, not similarity
 
             # append everything to the results list
-            results.append((pivot, inflection, sim_inflection))
+            results.append((pivot, inflection, sim_inflection, category))
 
         if model_name == "Word2Vec":
             print(f"Number of words not found in Word2Vec model: {not_found}")
 
     # create a df with the results list
     results_df = pd.DataFrame(results)
-    results_df.to_csv(f"py/results/{language}/{language}_{model_name.lower()}_results.csv", index=False, header=["pivot", "inflection", "P-I similarity"])
+    results_df.to_csv(f"results/{language}/{language}_{model_name.lower()}_results.csv", index=False, header=["pivot", "inflection", "P-I similarity", "category"])
     print("Results by row saved!")
 
     # similarities for mean
@@ -192,4 +197,4 @@ calculate_sims takes what choose_embeddings outputs and an argument of the file 
 # BERT
 # bert takes a really long time
 model, model_name, tokenizer, language = choose_embeddings("bert", "spa") # SPANISH
-calculate_sims(model, model_name, tokenizer, language, um_spa_small)
+calculate_sims(model, model_name, tokenizer, language, um_spa_small) # small dataset to test
