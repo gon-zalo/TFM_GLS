c1f115ae8a1632331d613f96bba76b28b4ce48a7
gon-zalo <147316353+gon-zalo@users.noreply.github.com>
2025-05-07 20:26:02 +0200

    added filters

--- py/filter_unimorph.py
+++ py/filter_unimorph.py
@@ -9,7 +9,12 @@ import pandas as pd
 spa_inf = "datasets/spa/spa.txt"
 pol_inf = "datasets/pol/pol.txt"
 
-def filter_inflections(data):
+# UNFILTERED DERIVATION DATASETS
+
+spa_der = pd.read_csv("datasets/spa/spa_derivations.txt", sep="\t", header=None, names=["pivot", "derivation", "category", "affix"])
+pol_der = pd.read_csv("datasets/pol/pol_derivations.txt", sep="\t", header=None, names=["pivot", "derivation", "category", "affix"])
+
+def clean_inflection(data):
     if data == spa_inf:
         language = "spa"
         print("Filtering Spanish data...")
@@ -45,5 +50,63 @@ def filter_inflections(data):
     df.to_csv(f"datasets/{language}/{language}_inflections.txt", sep="\t", index=False, header=False)
     print("Filtered data saved!")
 
-# filter_inflections(spa_inf)
-# filter_inflections(pol_inf)
+def filter_inflection(dataframe, language):
+
+    if language == "spa":
+        print("Filtering Spanish data...")
+    elif language == "pol":
+        print("Filtering Polish data...")
+
+    df = dataframe.copy()
+
+    # filtering data to only obtain presente, pret. impf. and futuro simple
+    if language == "spa":
+        present_df =  df[df["category"].str.contains("V;IND;PRS", na=False)] # presente
+        present_df = present_df.reset_index(drop=True) # reset index after filtering
+
+        past_df = df[df["category"].str.contains("V;IND;PST;IPFV", na=False)] # pret. impf.
+        past_df = past_df.reset_index(drop=True)
+
+        future_df = df[df["category"].str.contains("V;IND;FUT", na=False)] # futuro simple
+        future_df = future_df.reset_index(drop=True)
+
+    elif language == "pol":
+        present_df =  df[df["category"].str.contains("V;PRS", na=False)]
+        present_df = present_df.reset_index(drop=True)
+
+        past_df = df[df["category"].str.contains("V;PST", na=False)]
+        past_df = past_df.reset_index(drop=True)
+
+        future_df = df[df["category"].str.contains("V;FUT", na=False)]
+        future_df = future_df.reset_index(drop=True)
+
+    return present_df, past_df, future_df
+
+def filter_derivation(dataframe, language):
+
+    if language == "spa":
+        print("Filtering Spanish data...")
+    elif language == "pol":
+        print("Filtering Polish data...")
+
+    df = dataframe.copy()
+
+    # unique categories in the dataset
+    categories = df['category'].unique().tolist()
+    # empty dictionary to store filtered dataframes
+    filtered_dfs = {}
+
+    for category in categories:
+        # filter the dataframe for each unique category
+        filtered_df = df[df['category'] == category].reset_index(drop=True)
+        # append the filtered dataframe to the dictionary
+        filtered_df.columns = ['pivot', 'derivation', 'category', 'affix']
+        filtered_dfs[category] = filtered_df
+
+    # return a dictionary of 16 filtered dataframes
+    return filtered_dfs
+
+# filter_derivation(spa_der, 'spa')
+# filter_derivation(pol_der, 'pol')
+# clean_inflection(spa_inf)
+# clean_inflection(pol_inf)
